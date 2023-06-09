90281d50c35859725b6de89e57d69468ed27b3d8
paul-tes <paulasca8462@gmail.com>
2023-06-09 23:36:19 +0300

    Book Form

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,14 +1,15 @@
 const BookForm = () => (
-  <form className="book-fomr">
+  <form className="book-form">
     <h2>ADD NEW BOOK</h2>
     <div className="inputs">
-      <input type="text" placeholder="Book title" />
+      <input type="text" placeholder="Book title" className="book-title-input" />
       <select id="category" name="category" placeholder="Category">
-        <option value="fiction">Fiction</option>
+        <option disabled selected value="">Category</option>
         <option value="non-fiction">Non-Fiction</option>
         <option value="poetry">Poetry</option>
         <option value="biography">Biography</option>
       </select>
+      <button type="submit" className="add-book-btn">ADD BOOK</button>
     </div>
   </form>
 );
