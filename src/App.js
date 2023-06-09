0395df8015b0b626976e1cf795cd29487b387ede
paul-tes <paulasca8462@gmail.com>
2023-06-09 12:15:51 +0300

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <h1>App</h1>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
