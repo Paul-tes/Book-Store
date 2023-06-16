1b71bf41ed286a85364413ec787dda2d7bd8dbdc
Paul Tesfaye <86291500+Paul-tes@users.noreply.github.com>
2023-06-16 19:21:05 +0300

    add book, remove book, post book

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Home from './components/Pages/Home';
+import { fetchBooks } from './redux/books/booksSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [fetchBooks()]);
+
   return (
     <BrowserRouter>
       <Header />
